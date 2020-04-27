dc4499b70e98498c565a371eea2f1be26800274d
José Padilla <jpadilla@webapplicate.com>
2020-04-26 21:13:40 -0400

    Drop support for Python 2 (#14)

--- rest_framework_yaml/encoders.py
+++ rest_framework_yaml/encoders.py
@@ -2,13 +2,19 @@
 Helper classes for parsers.
 """
 from __future__ import unicode_literals
+
 import decimal
 import types
 
 from django.utils.encoding import force_str
 
 from .compat import (
-    yaml, yaml_represent_text, Hyperlink, OrderedDict, ReturnDict, ReturnList
+    Hyperlink,
+    OrderedDict,
+    ReturnDict,
+    ReturnList,
+    yaml,
+    yaml_represent_text,
 )
 
 
@@ -22,7 +28,7 @@ class SafeDumper(yaml.SafeDumper):
     """
 
     def represent_decimal(self, data):
-        return self.represent_scalar('tag:yaml.org,2002:str', force_str(data))
+        return self.represent_scalar("tag:yaml.org,2002:str", force_str(data))
 
     def represent_mapping(self, tag, mapping, flow_style=None):
         value = []
@@ -30,16 +36,21 @@ class SafeDumper(yaml.SafeDumper):
         if self.alias_key is not None:
             self.represented_objects[self.alias_key] = node
         best_style = True
-        if hasattr(mapping, 'items'):
+        if hasattr(mapping, "items"):
             mapping = list(mapping.items())
             if not isinstance(mapping, OrderedDict):
                 mapping.sort()
         for item_key, item_value in mapping:
             node_key = self.represent_data(item_key)
             node_value = self.represent_data(item_value)
-            if not (isinstance(node_key, yaml.ScalarNode) and not node_key.style):
+            if not (
+                isinstance(node_key, yaml.ScalarNode) and not node_key.style
+            ):
                 best_style = False
-            if not (isinstance(node_value, yaml.ScalarNode) and not node_value.style):
+            if not (
+                isinstance(node_value, yaml.ScalarNode)
+                and not node_value.style
+            ):
                 best_style = False
             value.append((node_key, node_value))
         if flow_style is None:
@@ -50,35 +61,25 @@ class SafeDumper(yaml.SafeDumper):
         return node
 
 
+SafeDumper.add_representer(decimal.Decimal, SafeDumper.represent_decimal)
+
 SafeDumper.add_representer(
-    decimal.Decimal,
-    SafeDumper.represent_decimal
+    OrderedDict, yaml.representer.SafeRepresenter.represent_dict
 )
 
 SafeDumper.add_representer(
-    OrderedDict,
-    yaml.representer.SafeRepresenter.represent_dict
-)
-
-SafeDumper.add_representer(
-    types.GeneratorType,
-    yaml.representer.SafeRepresenter.represent_list
+    types.GeneratorType, yaml.representer.SafeRepresenter.represent_list
 )
 
 if Hyperlink:
-    SafeDumper.add_representer(
-        Hyperlink,
-        yaml_represent_text
-    )
+    SafeDumper.add_representer(Hyperlink, yaml_represent_text)
 
 if ReturnDict:
     SafeDumper.add_representer(
-        ReturnDict,
-        yaml.representer.SafeRepresenter.represent_dict
+        ReturnDict, yaml.representer.SafeRepresenter.represent_dict
     )
 
 if ReturnList:
     SafeDumper.add_representer(
-        ReturnList,
-        yaml.representer.SafeRepresenter.represent_list
+        ReturnList, yaml.representer.SafeRepresenter.represent_list
     )
--- tests/test_renderers.py
+++ tests/test_renderers.py
@@ -2,13 +2,15 @@
 from __future__ import unicode_literals
 
 import unittest
-from io import BytesIO
+
 from decimal import Decimal
+from io import BytesIO
 
 from django.test import TestCase
+
 from rest_framework_yaml.compat import Hyperlink
-from rest_framework_yaml.renderers import YAMLRenderer
 from rest_framework_yaml.parsers import YAMLParser
+from rest_framework_yaml.renderers import YAMLRenderer
 
 
 class YAMLRendererTests(TestCase):
@@ -20,26 +22,26 @@ class YAMLRendererTests(TestCase):
         """
         Test basic YAML rendering.
         """
-        _yaml_repr = 'foo:\n- bar\n- baz\n'
+        _yaml_repr = "foo:\n- bar\n- baz\n"
 
-        obj = {'foo': ['bar', 'baz']}
+        obj = {"foo": ["bar", "baz"]}
 
         renderer = YAMLRenderer()
-        content = renderer.render(obj, 'application/yaml')
+        content = renderer.render(obj, "application/yaml")
 
-        self.assertEqual(content.decode('utf-8'), _yaml_repr)
+        self.assertEqual(content.decode("utf-8"), _yaml_repr)
 
     def test_render_and_parse(self):
         """
         Test rendering and then parsing returns the original object.
         IE obj -> render -> parse -> obj.
         """
-        obj = {'foo': ['bar', 'baz']}
+        obj = {"foo": ["bar", "baz"]}
 
         renderer = YAMLRenderer()
         parser = YAMLParser()
 
-        content = renderer.render(obj, 'application/yaml')
+        content = renderer.render(obj, "application/yaml")
         data = parser.parse(BytesIO(content))
         self.assertEqual(obj, data)
 
@@ -48,24 +50,31 @@ class YAMLRendererTests(TestCase):
         Test YAML decimal rendering.
         """
         renderer = YAMLRenderer()
-        content = renderer.render({'field': Decimal('111.2')}, 'application/yaml')
-        self.assertYAMLContains(content.decode('utf-8'), "field: '111.2'")
+        content = renderer.render(
+            {"field": Decimal("111.2")}, "application/yaml"
+        )
+        self.assertYAMLContains(content.decode("utf-8"), "field: '111.2'")
 
-    @unittest.skipUnless(Hyperlink, 'Hyperlink is undefined')
+    @unittest.skipUnless(Hyperlink, "Hyperlink is undefined")
     def test_render_hyperlink(self):
         """
         Test YAML Hyperlink rendering.
         """
         renderer = YAMLRenderer()
-        content = renderer.render({'field': Hyperlink('http://pépé.com?great-answer=42', 'test')}, 'application/yaml')
-        self.assertYAMLContains(content.decode('utf-8'), "field: http://pépé.com?great-answer=42")
+        content = renderer.render(
+            {"field": Hyperlink("http://pépé.com?great-answer=42", "test")},
+            "application/yaml",
+        )
+        self.assertYAMLContains(
+            content.decode("utf-8"), "field: http://pépé.com?great-answer=42"
+        )
 
     def assertYAMLContains(self, content, string):
-        self.assertTrue(string in content, '%r not in %r' % (string, content))
+        self.assertTrue(string in content, "%r not in %r" % (string, content))
 
     def test_proper_encoding(self):
-        _yaml_repr = 'countries:\n- United Kingdom\n- France\n- España'
-        obj = {'countries': ['United Kingdom', 'France', 'España']}
+        _yaml_repr = "countries:\n- United Kingdom\n- France\n- España"
+        obj = {"countries": ["United Kingdom", "France", "España"]}
         renderer = YAMLRenderer()
-        content = renderer.render(obj, 'application/yaml')
-        self.assertEqual(content.strip(), _yaml_repr.encode('utf-8'))
+        content = renderer.render(obj, "application/yaml")
+        self.assertEqual(content.strip(), _yaml_repr.encode("utf-8"))
